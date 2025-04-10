ff5777b10402ee894176cbfc941e3074c9c1ab0c
Erg0tt <142750254+Erg0tt@users.noreply.github.com>
2025-04-10 10:07:01 +0300

    Update Participant.cs

--- EventRegistry/Models/Participant.cs
+++ EventRegistry/Models/Participant.cs
@@ -1,7 +1,13 @@
-﻿public class Participant
+using System;
+public class Participant
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
     public bool IsConfirmed { get; set; }
+    public override string ToString()
+    {
+        return $"{Name}                     ({Email})";
+    }
+
 }
