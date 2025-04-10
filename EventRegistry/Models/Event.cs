5823e63f8bc7c0ebbef0b6592a708def56cedb73
Erg0tt <142750254+Erg0tt@users.noreply.github.com>
2025-04-10 10:06:45 +0300

    Update Event.cs

--- EventRegistry/Models/Event.cs
+++ EventRegistry/Models/Event.cs
@@ -1,10 +1,17 @@
-﻿using System;
+using System;
 namespace EventRegistrationApp
 {
     public class Event
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public string Format { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}     |     {Date:dd.MM.yyyy}     |     {Format}";
+        }
     }
+
 }
